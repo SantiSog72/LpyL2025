5bcdf64904a8e9f0a9150a22a60a57d9a8bbe101
santiago servin <santiagosservin@gmail.com>
2025-04-16 16:54:22 -0300

    reorganizacion tp2 tp3

---
+++ TP3_LPyL_santiago_servin_2025/Tutorial_Javascript/tutorial_ejem5_santiago_servin2025/javascript/index.js
@@ -0,0 +1,29 @@
+function calculos (){
+	let a, b, c =0, d= 0.0, e= "0", f = false, g, comparacion, resul;
+	let h = new array();
+	
+	<!--generacion de valores aleatorios para a y b-->
+	a = Math.round(Math.random() * (20 - 1) + 1);
+	b = Math.round (math.random() * (10 -1) +1);
+	
+	document.write("<section>");
+	document.write("<article>");
+	
+	document.write ("a = " + a + "br");
+	document.write ("b = " + b + "br");
+	
+	
+	<!--operaciones sobre a y b-->
+	resul = a+b;
+	document.write ("a + b = "+ result + "br");
+	result = a-b;
+	document.write ("a - b = " + result + "br");
+	result = a*b;
+	document.write ("a * b = " result + "br");
+	result = a/b;
+	document.write ("a / b = " +result + "br");
+	result = a%b;
+	document.write("a % b= " + result + "br");
+	
+	
+}
