357bf908b418ced84b7c532930c6743dead36f85
santiago servin <santiagosservin@gmail.com>
2025-04-18 20:58:13 -0300

    agregados tp3_tutorial_ejemp1:5

---
+++ TP3_LPyL_santiago_servin_2025/Tutorial_Javascript/tutorial_ejem4_santiago_servin2025/javascript/index.js
@@ -0,0 +1,10 @@
+function imprimir_cuerpo(){
+	document.write("<h1>archivo externo js</h1>");
+	document.write("<section>")
+	document.write("<article>")
+	document.write("<p>texto del articulo</p>")
+	
+	document.write("</article>")
+	document.write("</section>")
+
+}
--- TP3_LPyL_santiago_servin_2025/Tutorial_Javascript/tutorial_ejem5_santiago_servin2025/javascript/index.js
+++ TP3_LPyL_santiago_servin_2025/Tutorial_Javascript/tutorial_ejem5_santiago_servin2025/javascript/index.js
@@ -1,29 +1,30 @@
-function calculos (){
+function calculos ()
+{
 	let a, b, c =0, d= 0.0, e= "0", f = false, g, comparacion, resul;
-	let h = new array();
+	let h = new Array();
 	
-	<!--generacion de valores aleatorios para a y b-->
+	//generacion de valores aleatorios para a y b	
 	a = Math.round(Math.random() * (20 - 1) + 1);
-	b = Math.round (math.random() * (10 -1) +1);
+	b = Math.round (Math.random() * (10 -1) +1);
 	
 	document.write("<section>");
 	document.write("<article>");
 	
-	document.write ("a = " + a + "br");
-	document.write ("b = " + b + "br");
+	document.write ("a = " + a + "<br>");
+	document.write ("b = " + b + "<br>");
 	
-	
-	<!--operaciones sobre a y b-->
+	//operaciones sobre a y b
 	resul = a+b;
-	document.write ("a + b = "+ result + "br");
-	result = a-b;
-	document.write ("a - b = " + result + "br");
-	result = a*b;
-	document.write ("a * b = " result + "br");
-	result = a/b;
-	document.write ("a / b = " +result + "br");
-	result = a%b;
-	document.write("a % b= " + result + "br");
+	document.write ("a + b = "+ resul + "<br>");
+	resul= a-b;
+	document.write ("a - b = " + resul + "<br>");
+	resul = a*b;
+	document.write ("a * b = " resul + "<br>");
+	resul = a/b;
+	document.write ("a / b = " +resul + "<br>");
+	resul = a%b;
+	document.write("resto de (a/b) =" + result + "<br>");
 	
-	
+	document.write ("</article>");
+	document.write ("</section>");
 }
